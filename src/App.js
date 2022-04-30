48988d925f3831971a5494c3ce044650f088288b
mohammed.alnaggar <mohammed.alnaggar@vodafone.com>
2022-04-30 05:22:15 +0200

    test yarn app
    new app yarn

--- src/App.js
+++ src/App.js
@@ -1,71 +1,58 @@
 import React, { Component } from "react";
-import NavBar from "./components/navbar";
-import Counters from "./components/counters";
+import PhotoContextProvider from "./context/PhotoContext";
+import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
+import Header from "./components/Header";
+import Item from "./components/Item";
+import Search from "./components/Search";
+import NotFound from "./components/NotFound";
 
 class App extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 0 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 },
-    ],
-  };
-
-  handleIncrement = (counter) => {
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter);
-    counters[index] = { ...counters[index] };
-    counters[index].value++;
-    this.setState({ counters });
-  };
-
-  handleDecrement = (counter) => {
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter);
-    counters[index] = { ...counters[index] };
-    counters[index].value--;
-    this.setState({ counters });
-  };
-
-  handleReset = () => {
-    const counters = this.state.counters.map((c) => {
-      c.value = 0;
-      return c;
-    });
-    this.setState({ counters });
-  };
-
-  handleDelete = (counterId) => {
-    const counters = this.state.counters.filter((c) => c.id !== counterId);
-    this.setState({ counters });
-  };
-
-  handleRestart = () => {
-    window.location.reload();
+  // Prevent page reload, clear input, set URL and push history on submit
+  handleSubmit = (e, history, searchInput) => {
+    e.preventDefault();
+    e.currentTarget.reset();
+    let url = `/search/${searchInput}`;
+    history.push(url);
   };
 
   render() {
     return (
-      <div className="main__wrap">
-        <main className="container">
-          <div className="card__box">
-            <NavBar
-              totalCounters={
-                this.state.counters.filter((c) => c.value > 0).length
-              }
+      <PhotoContextProvider>
+        <HashRouter basename="/SnapScout">
+          <div className="container">
+            <Route
+              render={props => (
+                <Header
+                  handleSubmit={this.handleSubmit}
+                  history={props.history}
+                />
+              )}
             />
-            <Counters
-              counters={this.state.counters}
-              onReset={this.handleReset}
-              onIncrement={this.handleIncrement}
-              onDecrement={this.handleDecrement}
-              onDelete={this.handleDelete}
-              onRestart={this.handleRestart}
-            />
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={() => <Redirect to="/mountain" />}
+              />
+
+              <Route
+                path="/mountain"
+                render={() => <Item searchTerm="mountain" />}
+              />
+              <Route path="/beach" render={() => <Item searchTerm="beach" />} />
+              <Route path="/bird" render={() => <Item searchTerm="bird" />} />
+              <Route path="/food" render={() => <Item searchTerm="food" />} />
+              <Route
+                path="/search/:searchInput"
+                render={props => (
+                  <Search searchTerm={props.match.params.searchInput} />
+                )}
+              />
+              <Route component={NotFound} />
+            </Switch>
           </div>
-        </main>
-      </div>
+        </HashRouter>
+      </PhotoContextProvider>
     );
   }
 }
